a50ac704d69e444df21c2f490048628b6102cc42
marek <marek.cermi@gmail.com>
2021-07-18 23:25:16 +0200

    Correct htaccess, hidden login
    1) Corrected htaccess due to Wedos requirements
    2) Corrected NAV button names, login hidden

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,30 +13,31 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav.nav-link :href="route('home')" :active="request()->routeIs('home')">
-                        {{ 'Home' }}
+                        {{ 'Přehled' }}
                     </x-nav.nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav.nav-link>
-                        {{ 'News' }}
+                        {{ 'Novinky' }}
                     </x-nav.nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav.nav-link>
-                        {{ 'Calendar' }}
+                        {{ 'Serie' }}
                     </x-nav.nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav.nav-link>
-                        {{ 'Results' }}
+                        {{ 'Statistiky' }}
                     </x-nav.nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav.nav-link>
-                        {{ 'Regulations' }}
+                        {{ 'Pravidla' }}
                     </x-nav.nav-link>
                 </div>
             </div>
+            {{--
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
@@ -61,7 +62,6 @@
                         @guest()
                             <form method="GET" action="{{ route('login') }}">
                                 @csrf
-
                                 <x-nav.responsive-nav-link :href="route('login')"
                                                        onclick="event.preventDefault();
                                         this.closest('form').submit();">
@@ -81,7 +81,6 @@
                         @auth()
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
-
                                 <x-nav.responsive-nav-link :href="route('logout')"
                                                        onclick="event.preventDefault();
                                         this.closest('form').submit();">
@@ -92,6 +91,7 @@
                     </x-slot>
                 </x-nav.dropdown>
             </div>
+--}}
 
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
@@ -109,10 +109,31 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-nav.responsive-nav-link :href="route('home')" :active="request()->routeIs('home')">
-                {{ 'Home' }}
+                {{ 'Přehled' }}
+            </x-nav.responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-nav.responsive-nav-link>
+                {{ 'Novinky' }}
+            </x-nav.responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-nav.responsive-nav-link>
+                {{ 'Serie' }}
+            </x-nav.responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-nav.responsive-nav-link>
+                {{ 'Statstiky' }}
+            </x-nav.responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-nav.responsive-nav-link>
+                {{ 'Pravidla' }}
             </x-nav.responsive-nav-link>
         </div>
 
+        {{--
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
@@ -133,6 +154,15 @@
                             {{ 'Přihlášení' }}
                         </x-nav.responsive-nav-link>
                     </form>
+                    <form method="GET" action="{{ route('register') }}">
+                        @csrf
+
+                        <x-nav.responsive-nav-link :href="route('register')"
+                                                   onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                            {{ 'Registrace' }}
+                        </x-nav.responsive-nav-link>
+                    </form>
                 @endguest
                 @auth()
                     <form method="POST" action="{{ route('logout') }}">
@@ -147,5 +177,7 @@
                 @endauth
             </div>
         </div>
+        --}}
+
     </div>
 </nav>
