c5522047f8a5d5eb8bc903df38529cec495eb737
marek <marek.cermi@gmail.com>
2021-09-20 19:50:48 +0200

    Improved race result calculations
    1) New class for race result calculations race_results.php
    2) Implemented into RaceController.php

---
+++ app/custom/results/race_results.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\custom\results;
+
+use Illuminate\Support\Facades\DB;
+use phpDocumentor\Reflection\Types\Collection;
+
+class race_results{
+
+    public int $race_id;
+    public     $race_results;
+
+    /**
+     * @param $id 'Id of the race.'
+     */
+    public function __construct( $id ){
+        $this->race_id = $id;
+    }
+
+    /**
+     * Read race result from database. Race result is identified by race_id from constructor.
+     *
+     * @return race_results
+     */
+    public function read_results(){
+        $this->race_results = DB::table( 'race_result' )->where( 'race_id', '=', $this->race_id )
+                                ->join( 'valuation', 'race_result.valuation_id', '=', 'valuation.valuation_id' )
+                                ->leftJoin( 'penalty_flag', 'race_result.penalty_flag_id', '=', 'penalty_flag.penalty_flag_id' )
+                                ->leftJoin( 'participation', 'race_result.participation_id', '=', 'participation.participation_id' )
+                                ->leftJoin( 'user', 'participation.user_id', '=', 'user.user_id' )
+                                ->leftJoin( 'crew', 'participation.crew_id', '=', 'crew.crew_id' )
+                                ->leftJoin( 'team', 'participation.team_id', '=', 'team.team_id' )
+                                ->select( 'valuation.position as position', 'user.first_name as first_name', 'user.last_name as last_name',
+                                          'team.name as team', 'race_result.laps_completed as laps', 'race_result.best_lap as best_lap',
+                                          'race_result.consistency as consistency', 'race_result.pitstops_no as pits', 'valuation.points as points',
+                                          'penalty_flag.name as flag_name' )
+                                ->orderBy( 'position' )->get();
+        return $this;
+    }
+
+    /**
+     * For those who has penalty flag changes points to 0.
+     *
+     * @return $this
+     */
+    public function apply_penalty_flag(){
+        $modified = $this->race_results->map( function( $item ){
+            if($item->flag_name){
+                $item->points = 0;
+            }
+            return $item;
+        } );
+
+        $this->race_results = $modified;
+        return $this;
+    }
+
+}
