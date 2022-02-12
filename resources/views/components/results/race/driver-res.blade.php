612420836f20966bc1bf79f9dd2ec61d7560f4bb
Marek Čermák <marek.cermi@gmail.com>
2022-02-12 12:32:42 +0100

    Corrected responsive behaviour for phones

--- resources/views/components/results/race/driver-res.blade.php
+++ resources/views/components/results/race/driver-res.blade.php
@@ -3,18 +3,18 @@
 <div class="mx-auto w-full border-gray-300 border rounded-lg my-1 bg-gray">
     <div class="flex">
         <div class="flex flex-1 items-center">
-            <div class="flex-none w-20 px-3 text-center">
-                <p class="text-xl">{{$result->penalty_name ?? $result->res_position}}</p>
+            <div class="flex-none w-10 px-1 text-center sm:w-20 md:px-3">
+                <p class="text-sm font-medium md:text-xl">{{$result->penalty_name ?? $result->res_position}}</p>
             </div>
             <div class="hidden md:flex">
                 <img class="w-20 h-20" src="/public/storage/img/placeholders/avatar.png" alt="Driver photo">
             </div>
-            <div class="flex-1 px-3">
-                <p class="text-lg font-medium text-gray-900 truncate">{{$result->driver_first_name}} {{$result->driver_last_name}}</p>
-                <p class="text-sm text-gray-500 truncate">{{$result->team_name}}</p>
+            <div class="flex-1 px-1 md:px-3">
+                <p class="text-sm font-medium text-gray-900 md:text-lg">{{$result->driver_first_name}} {{$result->driver_last_name}}</p>
+                <p class="text-xs gray-500 md:text-sm">{{$result->team_name}}</p>
             </div>
         </div>
-        <div class="items-center hidden md:flex">
+        <div class="items-center hidden lg:flex">
             <div class="flex-none px-3">
                 <div class="text-base font-medium text-gray-900 truncate">{{$result->best_lap}}</div>
                 <div class="text-sm text-gray-400 truncate">Nejlepší kolo</div>
@@ -30,12 +30,12 @@
 
         </div>
         <div class="flex items-center text-center">
-            <div class="flex-none w-16 px-3 ">
-                <p class="text-base font-semibold">{{$result->penalty ? '+'.$result->penalty.'p':null}}</p>
+            <div class="flex-none w-8 px-1 md:px-3 md:w-16">
+                <p class="text-sm font-semibold md:text-base">{{$result->penalty ? '+'.$result->penalty.'p':null}}</p>
             </div>
-            <div class="flex-none w-20 px-3">
-                <span class="text-xl font-semibold">{{$result->points}}</span>
-                <span class="text-sm text-gray-500">b</span>
+            <div class="flex-none w-10 px-1 md:px-3 md:w-20">
+                <span class="text-base font-semibold md:text-xl">{{$result->points}}</span>
+                <span class="text-xs text-gray-500 md:text-sm">b</span>
             </div>
         </div>
     </div>
