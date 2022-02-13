21a011a7b22f28258b4e03b2d312e5b5e580faa4
Marek Čermák <marek.cermi@gmail.com>
2022-02-13 19:14:58 +0100

    Create universal simple site header and applied into race site.

--- resources/views/components/breadcrumb/body.blade.php
+++ resources/views/components/breadcrumb/body.blade.php
@@ -1,4 +1,4 @@
-<nav class="flex py-3 px-5 text-gray-700 bg-gray-50 rounded-lg border border-gray-200" aria-label="Breadcrumb">
+<nav class="flex p-3 text-gray-700" aria-label="Breadcrumb">
     <ol class="inline-flex  flex-wrap items-center space-x-1 md:space-x-3">
         {{$slot}}
     </ol>
