54966797dd9932d4590cecead8352df0ae6c726a
Marek Čermák <marek.cermi@gmail.com>
2022-02-13 21:01:15 +0100

    Corrected navigation bar for tablets

--- resources/views/components/header/simple.blade.php
+++ resources/views/components/header/simple.blade.php
@@ -1,7 +1,7 @@
 @props(['url'])
 
 <div class="container bg-white border">
-    <div class="flex flex-col-reverse h-32 md:h-52 bg-origin-border bg-cover bg-center"
+    <div class="flex flex-col-reverse h-32 lg:h-52 bg-origin-border bg-fix bg-cover bg-center"
          style="background-image: url({{$url}})">
         <div class="p-4 inline-block">
             <h1 class="font-bold text-gray-900 text-xl md:text-2xl lg:text-4xl">
