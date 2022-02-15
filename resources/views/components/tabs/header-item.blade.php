c5c7ab12ad06d936a62a5d55762595da300d5b5e
Marek Čermák <marek.cermi@gmail.com>
2022-02-15 21:57:25 +0100

    RACE-SITE modified tab buttons size

--- resources/views/components/tabs/header-item.blade.php
+++ resources/views/components/tabs/header-item.blade.php
@@ -2,7 +2,7 @@
 
 <li class="mr-2" role="presentation">
     <button
-        class="inline-block p-4 text-sm font-medium text-center text-gray-500 border-b-2 border-transparent hover:text-yellow-500 hover:border-yellow-500 {{$status}}"
+        class="inline-block px-2 py-4 text-sm font-medium text-center text-gray-500 border-b-2 border-transparent hover:text-yellow-500 hover:border-yellow-500 {{$status}}"
         id="{{$link.'-tab'}}" data-tabs-target="{{"#".$link}}" type="button" role="tab" aria-controls="{{$link}}" aria-selected="true">
         {{$slot}}
     </button>
