8ec34c23c34b011b3a95d89e950aa729274080e0
Marek Čermák <marek.cermi@gmail.com>
2022-02-15 17:56:17 +0100

    Added new tab into race resutl site for incidents.

--- resources/views/components/tabs/header-item.blade.php
+++ resources/views/components/tabs/header-item.blade.php
@@ -2,7 +2,7 @@
 
 <li class="mr-2" role="presentation">
     <button
-        class="inline-block p-4 text-sm font-medium text-center text-gray-500 border-b-2 border-transparent hover:text-yellow-500 hover:border-yellow-5000 {{$status}}"
+        class="inline-block p-4 text-sm font-medium text-center text-gray-500 border-b-2 border-transparent hover:text-yellow-500 hover:border-yellow-500 {{$status}}"
         id="dashboard-tab" data-tabs-target="{{"#".$link}}" type="button" role="tab" aria-controls="dashboard" aria-selected="true">
         {{$slot}}
     </button>
